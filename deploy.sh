8e88a44cc74a676b535b35afbd4484b19da0935b
Flynn <crf204@gmail.com>
2019-08-17 19:22:47 -0400

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 set -e
 export VERSION=$(poetry run python -c "import databricks_api; print(databricks_api.__version__.__version__)")
 poetry build
-twine upload dist/databricks_api-${VERSION}*
+poetry run twine upload dist/databricks_api-${VERSION}*
 git tag -a ${VERSION} -m "${VERSION}"
 git push --tags
