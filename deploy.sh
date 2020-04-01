c5f4fc4896906cda8c5a2bd185be219beb7075d1
flynn <crf204@gmail.com>
2020-04-01 19:42:57 -0400

    better deploy

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-export VERSION=$(poetry run python -c "import databricks_api; print(databricks_api.__version__)")
-poetry build
-poetry run twine upload dist/databricks_api-${VERSION}*
-git tag -a ${VERSION} -m "${VERSION}"
-git push --tags
--- update.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-poetry update databricks_cli
-poetry run python generate_docs.py
