d351207b64b3f4c4e11042341699a7010433d6d6
flynn <crf204@gmail.com>
2020-09-23 18:59:08 -0400

    fix version and release cmd

--- databricks_api/_version.py
+++ databricks_api/_version.py
@@ -2,7 +2,7 @@
 __title__ = "databricks-api"
 __description__ = "Databricks API client interface."
 __url__ = "https://github.com/crflynn/databricks-api"
-__version__ = "0.4.0"
+__version__ = "0.5.1"
 __author__ = "Christopher Flynn"
 __author_email__ = "crf204@gmail.com"
 __license__ = "MIT"
