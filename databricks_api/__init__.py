bf8bd209fed896a7e1f41dd40d2817a30e089346
flynn <crf204@gmail.com>
2022-06-17 17:22:46 -0400

    cleanup init/version

--- databricks_api/__init__.py
+++ databricks_api/__init__.py
@@ -1,9 +1,3 @@
 from databricks_api.databricks import DatabricksAPI
-from databricks_api._version import __author__
-from databricks_api._version import __author_email__
-from databricks_api._version import __copyright__
-from databricks_api._version import __description__
-from databricks_api._version import __license__
-from databricks_api._version import __title__
-from databricks_api._version import __url__
 from databricks_api._version import __version__
+from databricks_cli.version import version as __cli_version__
