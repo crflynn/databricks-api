868ce04b4236ce2fd1e899b8ac5dfd2fd3a853f3
Flynn <crflynn@users.noreply.github.com>
2022-06-17 17:23:20 -0400

    cleanup init/version (#31)

--- test.py
+++ test.py
@@ -9,3 +9,10 @@ HOST = os.environ["DATABRICKS_HOST"]
 def test_databricks_api():
     db = DatabricksAPI(host=HOST, token=TOKEN)
     print(db.__dict__)
+
+
+def test_cli_version():
+    from databricks_api import __cli_version__
+    from databricks_cli.version import version
+
+    assert __cli_version__ == version
